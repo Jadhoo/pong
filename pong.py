d52406e66e6eee0a9eae88ad9f7f5fbdabc85d12
Jadhoo <apvaishak9@gmail.com>
2020-11-09 19:43:44 +0530

    add left and right paddles

--- pong.py
+++ pong.py
@@ -9,5 +9,23 @@ wn.bgcolor("black")
 wn.setup(width = 800, height = 600)
 wn.tracer(10)
 
+# left paddle
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.penup()
+paddle_a.setposition(-350, 0)
+paddle_a.shapesize(stretch_wid = 5, stretch_len = 1)
+
+# right paddle
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.penup()
+paddle_b.setposition(350, 0)
+paddle_b.shapesize(stretch_wid = 5, stretch_len = 1)
+
 while(True):
     wn.update()
